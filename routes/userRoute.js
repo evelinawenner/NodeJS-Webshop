2b04bc5a4104bc38e18807b73d8e8f1cb680845f
Lovisa Bohman <lovisabo@hotmail.com>
2021-03-11 16:28:06 +0100

    Add really basic route setup

---
+++ routes/userRoute.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/register");
+
+router.post("/register");
+
+router.get("/login");
+
+router.post("/login");
+
+module.exports = router;
+
