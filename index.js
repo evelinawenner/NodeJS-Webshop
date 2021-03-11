3ad2e957c6ca30f70f38ed57422231f2c0a40951
Evelina Wenner <evelina.nilssonvenner@medieinstitutet.se>
2021-03-11 16:19:00 +0100

    database setup

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-hej
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
+require("dotenv").config();
+const app = express();
+
+//app middlewares
+app.set("view engine", "ejs");
+app.use("/static", express.static("public"));
+
+
+//connection to db
+const options = { 
+    useNewUrlParser: true,
+useUnifiedTopology: true,
+useFindAndModify: false,
+useCreateIndex: true,
+}
+
+mongoose.connect(process.env.DB_CONNECT, options, (err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+console.log("Connected to db!");
+app.listen(5001, () => console.log("Server Up and running"));
+});
