00273e43ff8d59853fc5e470737d4313b15e91ac
Lovisa Bohman <lovisabo@hotmail.com>
2021-03-12 15:01:07 +0100

    Add loginController to userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,13 +2,17 @@ const express = require("express");
 
 const router = express.Router();
 
+const { loginCheck } = require("../controller/loginController");
+
+
+//Routes
 router.get("/register");
 
 router.post("/register");
 
 router.get("/login");
 
-router.post("/login");
+router.post("/login", loginCheck);
 
 module.exports = router;
 
