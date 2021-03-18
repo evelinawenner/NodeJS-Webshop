1a4e541561a9d7003fadf5037d0c096711129f08
Lei Klingberg <zhang.lei92@outlook.com>
2021-03-18 11:15:36 +0100

    add reset js

--- controller/registerController.js
+++ controller/registerController.js
@@ -2,14 +2,14 @@ const User = require("../models/user");
 const bcrypt = require("bcrypt");
 
 const registerRender = async (req, res) => {
-    res.render("register.ejs", { err:"" });
+    res.render("register.ejs", { err:" "});
 }
 
 const registerSubmit = async (req, res) => {
     const { name, email, password } = req.body;
 
     try {
-        const salt = await bcrypt.genSalt(11);
+        const salt = await bcrypt.genSalt(12);
         const hashedPassword = await bcrypt.hash(password, salt);
 
         await new User({
@@ -17,10 +17,10 @@ const registerSubmit = async (req, res) => {
             email: email,
             password: hashedPassword
         }).save();
-        return res.send("Kontot registrerat!");
+        return res.redirect("/login");
     }
     catch(err) {
-        if(err) return res.send({ err:err });
+        if(err) return res.render("register.ejs", { err:err });
     }
 }
 
